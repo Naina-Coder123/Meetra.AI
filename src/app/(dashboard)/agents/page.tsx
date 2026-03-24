a1226c741b9ffc9c8b669ddf94178e04e934470e
Naina Mishra <2k23.csaiml2310558@gmail.com>
2026-03-24 22:39:05 +0530

    agent-form

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,20 +1,43 @@
-import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/ui/views/agents-view"
-import { getQueryClient,trpc } from "@/trpc/server";
-import { dehydrate,HydrationBoundary } from 
-"@tanstack/react-query";
-import { ErrorBoundary } from "react-error-boundary"
 import { Suspense } from "react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { ErrorBoundary } from "react-error-boundary";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { auth } from "@/lib/auth";
+import { getQueryClient, trpc } from "@/trpc/server";
+
+import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
+import { 
+  AgentsView, 
+  AgentsViewError, 
+  AgentsViewLoading
+} from "@/modules/agents/ui/views/agents-view";
+
 const Page = async () => {
-    const queryClient=getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions())
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+
   return (
-   <HydrationBoundary state={dehydrate(queryClient)}>
+    <>
+      <AgentsListHeader />
+      <HydrationBoundary state={dehydrate(queryClient)}>
         <Suspense fallback={<AgentsViewLoading />}>
           <ErrorBoundary fallback={<AgentsViewError />}>
             <AgentsView />
           </ErrorBoundary>
         </Suspense>
       </HydrationBoundary>
+    </>
   );
-}
+};
+ 
 export default Page;
